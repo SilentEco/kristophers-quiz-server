a0b2ce75db41cc9987eba4f86660538257146a8a
Kristopher Kram <creambulle@gmail.com>
2021-12-07 13:40:30 +0100

    fix

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "admin",
-  host: "localhost",
+  user: "pxduaagnfrxlvn",
+  password: "3e68fab1dec376e33489405d19fabe3fb52ab27092b07f4f21277a45fb1ebb41",
+  host: "ec2-52-17-1-206.eu-west-1.compute.amazonaws.com",
   post: 5432,
-  database: "quiz",
+  database: "dm6gfrjulannb",
 });
 
 module.exports = pool;
