5af966ff764281e479e45e0e8fbba121befdd6bf
Kristopher Kram <creambulle@gmail.com>
2021-12-07 14:57:47 +0100

    teeeets

--- db.js
+++ db.js
@@ -1,11 +1,18 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
-const pool = new Pool({
-  user: "pxduaagnfrxlvn",
-  password: "3e68fab1dec376e33489405d19fabe3fb52ab27092b07f4f21277a45fb1ebb41",
-  host: "ec2-52-17-1-206.eu-west-1.compute.amazonaws.com",
-  post: 5432,
-  database: "dm6gfrjulannb",
-});
+const devConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  post: process.env.PG_POST,
+  database: process.env.PG_DATABASE,
+};
+
+const proConfig = {
+  connectionstring: process.env.DATABASE_URL,
+};
+
+const pool = new Pool(proConfig);
 
 module.exports = pool;
