0fa6c2fae9813cf66002e5f654050e3bffd18c7d
Kristopher Kram <creambulle@gmail.com>
2021-12-09 16:58:11 +0100

    nytt test

--- db.js
+++ db.js
@@ -30,7 +30,7 @@ const devConfig = {
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   host: process.env.PG_HOST,
-  post: process.env.PG_POST,
+  port: process.env.PG_PORT,
   database: process.env.PG_DATABASE,
 };
 
